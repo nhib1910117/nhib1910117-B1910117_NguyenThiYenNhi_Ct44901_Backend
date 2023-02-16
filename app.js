9c1669041e8d15e4b9bcbae81653d8737b81f562
Nhib1910117 <nhib1910117@student.ctu.edu.vn>
2023-02-16 22:03:52 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 
-
+const contactsRouter = require("./app/routes/contact.route");
 const app = express();
-
 
 app.use(cors());
 app.use(express.json());
@@ -12,6 +11,6 @@ app.get("/", (req, res) =>{
     res.json({ message: "Welcome to contact book application." });
 
 });
-
+app.use("/api/contacts", contactsRouter);
 
 module.exports = app;
