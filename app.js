78017ffabcb5a1e2f3a151b4516e15c1e532b25d
Nhib1910117 <nhib1910117@student.ctu.edu.vn>
2023-02-16 22:17:13 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require("express");
 const cors = require("cors");
 
 const contactsRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
 const app = express();
+
 
 app.use(cors());
 app.use(express.json());
@@ -12,5 +14,15 @@ app.get("/", (req, res) =>{
 
 });
 app.use("/api/contacts", contactsRouter);
+app.use("/api/contacts", contactsRouter);
 
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resouce not found"));
+});
+
+app.use((error, req, res, next) => {
+    return res.status(error.statusCode || 500).json({
+        message: error.message || "Internal Server Error",
+    });
+});
 module.exports = app;
